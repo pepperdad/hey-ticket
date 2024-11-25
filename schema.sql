0179cc54dc82bab91fc20cf92c2931ebd35d15b4
jerry <becomegoodguy@gmail.com>
2024-11-25 18:48:26 +0900

    feat: season, archive 테이블 추가

--- schema.sql
+++ schema.sql
@@ -3,3 +3,27 @@ CREATE TABLE IF NOT EXISTS emoji_daily (
     sent_count INTEGER DEFAULT 0,
     received_count INTEGER DEFAULT 0
 );
+
+CREATE TABLE IF NOT EXISTS season_info (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    season_name TEXT NOT NULL,    
+    start_date TEXT NOT NULL,           
+    end_date DATE DEFAULT NULL            
+);
+
+CREATE TABLE IF NOT EXISTS emoji_season (
+    season_id INTEGER NOT NULL,         
+    user_id TEXT NOT NULL,             
+    sent_count INTEGER DEFAULT 0,        
+    received_count INTEGER DEFAULT 0,    
+    UNIQUE(season_id, user_id),         
+    FOREIGN KEY (season_id) REFERENCES season_info(id)
+);
+
+CREATE TABLE IF NOT EXISTS emoji_season_archive (
+    season_id INTEGER NOT NULL,         
+    user_id TEXT NOT NULL,             
+    sent_count INTEGER DEFAULT 0,        
+    received_count INTEGER DEFAULT 0,    
+    FOREIGN KEY (season_id) REFERENCES season_info(id)
+);
