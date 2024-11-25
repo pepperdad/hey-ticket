4434bd0cb4962483f9b54341ae136b109586dc20
jerry <becomegoodguy@gmail.com>
2024-11-25 18:53:15 +0900

    feat: reset sql문 추가 - npx wrangler d1 execute <DB-NAME> --file=reset.sql --remote

---
+++ reset.sql
@@ -0,0 +1,9 @@
+-- DELETE FROM emoji_daily;
+-- DELETE FROM emoji_season;
+-- DELETE FROM emoji_season_archive;
+-- DELETE FROM season_info;
+
+DROP TABLE IF EXISTS emoji_daily;
+DROP TABLE IF EXISTS emoji_season;
+DROP TABLE IF EXISTS emoji_season_archive;
+DROP TABLE IF EXISTS season_info;
