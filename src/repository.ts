9c87e15526b25ed40a7402f7c0495c753b9e7993
jerry <becomegoodguy@gmail.com>
2024-11-25 18:51:02 +0900

    feat: 이모지 전송 시, season 테이블에도 추가하도록 변경

--- src/repository.ts
+++ src/repository.ts
@@ -22,6 +22,39 @@ export class Repository {
     return Math.max(0, remainingQuota);
   }
 
+  async getCurrentSeasonId() {
+    const seasonInfo = await this.db
+      .selectFrom("season_info")
+      .select("id")
+      .orderBy("id", "desc")
+      .limit(1)
+      .executeTakeFirst();
+
+    let seasonId = seasonInfo?.id;
+    if (!seasonId) {
+      seasonId = await this.createSeason();
+    }
+
+    return seasonId;
+  }
+
+  async createSeason(): Promise<number> {
+    const seasonName = `Season ${new Date().toISOString().slice(0, 10)}`;
+    const startDate = new Date().toISOString().split("T")[0];
+
+    await this.db
+      .insertInto("season_info")
+      .values({
+        season_name: seasonName,
+        start_date: startDate,
+      })
+      .execute();
+
+    const seasonId = await this.getCurrentSeasonId();
+
+    return seasonId;
+  }
+
   /**
    * @desc 사용자에게 이모지를 보냅니다.
    */
@@ -32,6 +65,7 @@ export class Repository {
     }
     count = Math.min(quota, count);
 
+    // daily 테이블에 추가
     await this.db
       .insertInto("emoji_daily")
       .values({ user_id: giver, sent_count: count, received_count: 0 })
@@ -48,6 +82,37 @@ export class Repository {
         oc.column("user_id").doUpdateSet({
           received_count: sql`received_count + ${count}`,
         })
+      )
+      .execute();
+
+    // season 테이블에 추가
+    const seasonId = await this.getCurrentSeasonId();
+    await this.db
+      .insertInto("emoji_season")
+      .values({
+        season_id: seasonId,
+        user_id: giver,
+        sent_count: count,
+        received_count: 0,
+      })
+      .onConflict((oc) =>
+        oc
+          .columns(["season_id", "user_id"])
+          .doUpdateSet({ sent_count: sql`sent_count + ${count}` })
+      )
+      .execute();
+    await this.db
+      .insertInto("emoji_season")
+      .values({
+        season_id: seasonId,
+        user_id: receiver,
+        sent_count: 0,
+        received_count: count,
+      })
+      .onConflict((oc) =>
+        oc
+          .columns(["season_id", "user_id"])
+          .doUpdateSet({ received_count: sql`received_count + ${count}` })
       )
       .execute();
 
--- src/types/index.ts
+++ src/types/index.ts
@@ -12,4 +12,22 @@ interface Database {
     sent_count: number;
     received_count: number;
   };
+  season_info: {
+    id?: number;
+    season_name: string;
+    start_date: string;
+    end_date?: string;
+  };
+  emoji_season: {
+    season_id: number;
+    user_id: string;
+    sent_count: number;
+    received_count: number;
+  };
+  emoji_season_archive: {
+    season_id: number;
+    user_id: string;
+    sent_count: number;
+    received_count: number;
+  };
 }
